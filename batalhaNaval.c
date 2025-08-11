dd04a4613d658fc187124eca33235016b772a10c
kottybarn3567 <kaualopes3567@gmail.com>
2025-08-11 13:02:04 +0000

    NIVEL NOVATO: fiz a parte de alocar os navios dentro da matriz, usei ambos de tamanho 3 usei laços para preencher os espaços correspondetes aos dos navios com o nunero 3

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -10,31 +10,38 @@ int main() {
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    // ====================================================================================================================
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    /*
+    int campoBatalhaNaval[10][10];
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) { // vai de 0 ate 9, entao nao podemos usar <=
+                campoBatalhaNaval[i][j] = 0;
+            }
+        }
+    */
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    // MANEIRA SIMPLIFICADA
+
+    int campoBatalhaNaval[10][10] = {0};
+
+    for (int x = 2; x < 2 + 3; x++) {
+        campoBatalhaNaval[x][1] = 3;
+    }
+
+    for (int x = 6; x < 6 + 3; x++) {
+        campoBatalhaNaval[8][x] = 3;
+    }
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            printf("%d \t", campoBatalhaNaval[i][j]);
+        }
+        printf("\n\n");
+    }
 
     return 0;
 }
+
+// ====================================================================================================================
